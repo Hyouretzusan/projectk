9f828ffe4811755d2e4433a88c72bddf55c6ac66
Miguel Carmona <miguelcar18@gmail.com>
2017-05-03 16:52:10 -0400

    app folder for project k

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ProjectK\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    
+public function index()
+	{
+
+		return  view('layouts.mainview');
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
